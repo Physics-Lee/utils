d91f0495586e74dfc437011b11e23fab69612563
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-10-15 09:28:28 +0800

    init

---
+++ general/get_all_files_of_a_certain_name_pattern_in_a_rootpath.m
@@ -0,0 +1,30 @@
+% get all files of a centain type in a rootpath and its sub-folders.
+%
+% 2023-09-26, Yixuan Li
+%
+
+function list = get_all_files_of_a_certain_name_pattern_in_a_rootpath(rootpath, filetype)
+
+% Obtain all the subfolders recursively
+folders = string(split(genpath(rootpath), pathsep));
+folders = folders(1:length(folders)-1);
+
+% Initialize an empty cell array to hold filenames
+list = {};
+
+% Convert string array to cell array for the loop
+folders = cellstr(folders);
+
+% Loop over all folders
+for i = 1:length(folders)
+    % Get a list of all files in the folder that match the filetype
+    files = dir(fullfile(folders{i}, filetype));
+    
+    % Loop over all files and append to the list
+    for j = 1:length(files)
+        list{end+1} = fullfile(files(j).folder, files(j).name);
+    end
+    
+end
+
+end
---
+++ general/get_all_folders_of_a_certain_name_pattern_in_a_rootpath.m
@@ -0,0 +1,32 @@
+% get all folders of a certain name pattern in a rootpath and its sub-folders.
+%
+% 2023-10-07, Yixuan Li (Modified by ChatGPT)
+%
+
+function list = get_all_folders_of_a_certain_name_pattern_in_a_rootpath(rootpath, folderPattern)
+
+% Obtain all the subfolders recursively
+folders = string(split(genpath(rootpath), pathsep));
+folders = folders(1:length(folders)-1);
+
+% Initialize an empty cell array to hold folder names
+list = {};
+
+% Convert string array to cell array for the loop
+folders = cellstr(folders);
+
+% Loop over all folders
+for i = 1:length(folders)
+    % Get a list of all items in the folder
+    allItems = dir(folders{i});
+    
+    % Filter for items that are folders and match the folderPattern
+    matchingFolders = allItems([allItems.isdir] & contains({allItems.name}, folderPattern));
+    
+    % Loop over all matching folders and append to the list
+    for j = 1:length(matchingFolders)
+        list{end+1} = fullfile(matchingFolders(j).folder, matchingFolders(j).name);
+    end
+end
+
+end
