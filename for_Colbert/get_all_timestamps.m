133bfbd245aff48802497c1f2ab4466555805179
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2024-07-17 16:32:53 +0800

    add getting timestamps

---
+++ for_Colbert/get_all_timestamps.m
@@ -0,0 +1,3 @@
+function timestamps = get_all_timestamps(mcd)
+timestamps = [mcd.TimeElapsed];
+end
--- for_Colbert/get_lengths.m
+++ for_Colbert/get_lengths.m
@@ -1,13 +1,13 @@
-% Get lengths of all centerline of a single mcd.mat.
+% Get lengths (mm) of all centerline of a single mcd.mat.
 %
 % 2023-10-13, Yixuan Li
 %
 
-function lengths_of_centerlines = get_lengths(all_centerline)
-n_frames = length(all_centerline);
+function lengths_of_centerlines = get_lengths(centerlines_lab)
+n_frames = length(centerlines_lab);
 lengths_of_centerlines = zeros(n_frames,1);
 for i = 1:n_frames
-    centerline = all_centerline{i,1};
+    centerline = centerlines_lab{i,1};
     lengths_of_centerlines(i) = calculate_the_length_of_a_centerline(centerline);
 end
 end
