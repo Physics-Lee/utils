79a3a5a31b74d82d32456ea59223e99e0c6dceea
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2025-02-19 16:27:02 +0800

    explain why my_diff work

--- for_taxis_project/get_taxis_type_by_full_path.m
+++ for_taxis_project/get_taxis_type_by_full_path.m
@@ -8,9 +8,9 @@ function option_taxis = get_taxis_type_by_full_path(full_path)
 if contains(full_path,'NC')
     option_taxis = "NC";
 elseif contains(full_path,'NT')
-    % option_taxis = "NT";
-    option_taxis = "Or"; % for comparing RIA NT and RIA Or
-elseif contains(full_path,'Or') || contains(full_path,'ortho')
+    option_taxis = "NT";
+    % option_taxis = "Or"; % for comparing RIA NT and RIA Or
+elseif contains(full_path,'Or') || contains(full_path,'ortho') || contains(full_path,'OR')
     option_taxis = "Or";
 elseif contains(full_path,'Ctl') || contains(full_path,'ctl')
     option_taxis = "Ctl";
--- for_taxis_project/my_diff.m
+++ for_taxis_project/my_diff.m
@@ -1,6 +1,12 @@
 % return theta_2 - theta_1
 % theta_2 is a number
 %
+% Think About Geometry, this function is asking, rotate how much to get theta_2
+% from theta_1? And this "how much" belongs to [-pi, +pi].
+%
+% This function is equal to `diff = mod(theta_in - theta_ideal + pi, 2*pi)
+% - pi;`
+%
 % 2023-09-28, Yixuan Li
 %
 
